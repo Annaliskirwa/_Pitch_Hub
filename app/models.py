812bc4d3fb65524243063f13786d8a87c663e343
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-08 17:30:31 +0300

    Verify password functions

--- app/models.py
+++ app/models.py
@@ -21,3 +21,18 @@ class User(UserMixin, db.Model):
   downvotes = db.relationship('Downvote', backref='user', lazy='dynamic')
   bio = db.Column(db.String(255))
   profile_pic_path = db.Column(db.String())
+
+
+@property
+def password(self):
+    raise AttributeError('You cannot read the password attribute')
+
+@password.setter
+def password(self, password):
+    self.pass_secure = generate_password_hash(password)
+
+def verify_password(self, password):
+    return check_password_hash(self.pass_secure, password)
+
+def __repr__(self):
+    return f'{self.username}'
