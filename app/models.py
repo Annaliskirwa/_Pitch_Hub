e39d156d8ce3f41ff4432bc9ebe8f2b9e60d28bc
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-08 16:54:40 +0300

    Creates a user class

--- app/models.py
+++ app/models.py
@@ -7,3 +7,17 @@ from werkzeug.security import generate_password_hash, check_password_hash
 @login_manager.user_loader
 def load_user(user_id):
   return User.query.get(int(user_id))
+
+class User(UserMixin, db.Model):
+
+  __tablename__ = 'users'
+  id = db.Column(db.Integer, primary_key = True)
+  username = db.Column(db.String(255))
+  email = db.Column(db.String(255), unique = True, index = True)
+  pass_secure = db.Column(db.String(255))
+  pitch = db.relationship('Pitch', backref='user', lazy='dynamic')
+  comment = db.relationship('Comment', backref='user', lazy='dynamic')
+  upvotes = db.relationship('Upvote', backref='user', lazy='dynamic')
+  downvotes = db.relationship('Downvote', backref='user', lazy='dynamic')
+  bio = db.Column(db.String(255))
+  profile_pic_path = db.Column(db.String())
