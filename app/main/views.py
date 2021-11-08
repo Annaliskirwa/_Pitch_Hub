ba509e762aea1234dff7d8081a1378c739da48b9
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-08 09:11:34 +0300

    View upvote function that returns the upvotes on a particular quote

--- app/main/views.py
+++ app/main/views.py
@@ -64,3 +64,20 @@ def new_comment(pitch_id):
 
   all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
   return render_template('comments.html', form = form, comment = all_comments, pitch = pitch)
+
+@main.route('/pitch/upvote/<int:pitch_id>/upvote', methods = ['GET', 'POST'])
+@login_required
+def upvote(pitch_id):
+  """
+  View upvote function that returns the upvotes on a particular quote
+  """
+  pitch = Pitch.query.get(pitch_id)
+  user = current_user
+  pitch_upvotes = Upvote.query.filter_by(pitch_id=pitch_id)
+
+  if Upvote.query.filter(Upvote.user_id==user.id, Upvote.pitch_id==pitch_id).first():
+    return redirect(url_for('main.index'))
+
+  new_upvote = Upvote(pitch_id=pitch_id, user = current_user)
+  new_upvote.save_upvotes()
+  return redirect(url_for('main.index'))
