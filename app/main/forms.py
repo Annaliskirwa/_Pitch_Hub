4859d55d1e5e8b9c744ef85083ffdde70bfdf303
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-12 23:28:18 +0300

    Creating a pitch form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,11 +1,13 @@
+from typing import Text
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, RadioField
+from wtforms.fields.core import Label
 from wtforms.validators import Required, Email, EqualTo
 from wtforms import ValidationError
 
 class PitchForm(FlaskForm):
   title = StringField('Title', validators = [Required()])
-  description = TextAreaField("What category does your pitch fall under?", validators = [Required()])
+  description = TextAreaField("Write here your pitch",validators = [Required()])
   category = RadioField('Label', choices = [('promotionpitch', 'Promotion Pitch'), ('interviewpitch', 'Interview Pitch'), ('pickuplines', 'Pick-Up Lines'), ('productpitch', 'Product Pitch')], validators = [Required()])
   submit = SubmitField('Submit')
 
